0b9aa7a0fb7e71e9f3a69124a2437ed92744ca97
damocles <mrding1236@163.com>
2016-04-10 16:17:03 +0800

    add login code 2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,34 +19,69 @@ function init() {
 
 	// 登录
 	(function() {
-		var oFocus  = document.getElementById('focus'),
-			oForm   = document.getElementById('form'),
-			aInput  = oForm.getElementsByTagName('input');
+		var oSubmit = document.getElementById('submit'),
+			oForm = document.getElementById('form'),
+			aInput = oForm.getElementsByTagName('input'), 
+			aLabel = oForm.getElementsByTagName('label'),
+			oAttention = GLOBAL.Dom.getElementsByClassName('attention')[0],
 			oCancel = GLOBAL.Dom.getElementsByClassName('cancel')[0],
-			oSignIn = GLOBAL.Dom.getElementsByClassName('sign-in')[0],
-			oSubmit = GLOBAL.Dom.getElementsByClassName('submit')[0];
+			oSignIn = GLOBAL.Dom.getElementsByClassName('sign-in')[0];
 
-		oFocus.onclick = function() {
+		oAttention.onclick = function() {
 			if (!GLOBAL.Cookie.getCookie('loginSuc')) {
 				oSignIn.style.display = 'block';
 			} else {
-				oFocus.value = '已关注';
-				oFocus.disabled = false;
-				// cancel.style.display = 'block';
+				oAttention.value = '已关注';
+				oAttention.disabled = true;
+				oAttention.className = 'active';
+				oCancel.style.display = 'block';
 			}
 		}
 
 		oSubmit.onclick = function() {
-			var userName = hex_md5(aInput[0]),
-				passWord = hex_md5(aInput[1]);
+			var userName1 = hex_md5(aInput[0]),
+				passWord1 = hex_md5(aInput[1]);
 
-			GLOBAL.Ajax.get('http://study.163.com/webDev/login.htm', {userName:userName, password:passWord}, function(i) {
+			GLOBAL.Ajax.get('http://study.163.com/webDev/login.htm', {userName:userName1,password:passWord1}, function(i) {
 				if (i == '1') {
-					// ...
+					oSignIn.style.display = 'none';
+					GLOBAL.Cookie.setCookie('loginSuc', '1', 14);
+					GLOBAL.Ajax.get('http://study.163.com/webDev/attention.htm', '' , function(i) {
+						if (i == '1') 	{
+							GLOBAL.Cookie.setCookie('followSuc', '1', 14);
+							oAttention.value = '已关注';
+							oAttention.disabled = true;
+							oAttention.className = 'active';
+							oCancel.style.display = 'block';	
+						}
+					});
 				} else {
 					alert("账号或密码输入错误，请重新输入！");
 				}
-			})
+			});
 		}
+
+		function hiddenText(i) {
+			aInput[i].onfocus = function() {
+				aLabel[i].style.display = 'none';
+			}
+
+			aInput[i].onblur = function() {
+				if (this.value === '') {
+					aLabel[i].style.display = 'block';
+				}
+			}
+		}
+
+		hiddenText(0);
+		hiddenText(1);
+
 	})();
+
+	// 轮播图
+	(function() {
+		
+	})();
+
+		
 }
